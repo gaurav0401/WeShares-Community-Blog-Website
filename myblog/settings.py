addcf88bf8e7ed7ada59deb7fab27068711f2a64
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-09-10 21:17:31 +0530

    new

--- myblog/settings.py
+++ myblog/settings.py
@@ -89,13 +89,23 @@ DATABASES = {
 
 
     
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get("DB_NAME"),
+    #     'USER': os.environ.get("DB_USER"),
+    #     'PASSWORD': os.environ.get("DB_PASSWORD"),
+    #     'HOST': os.environ.get("DB_HOST"),
+    #     'PORT': os.environ.get("DB_PORT"),
+    # },
+
+
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'gaurav0401',
+        'PASSWORD': '@Ab22558800$',
+        'HOST': 'ep-broken-star-95580647-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT': '5432',
     }
 
 }
